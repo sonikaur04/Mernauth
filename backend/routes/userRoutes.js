f3447998b22049503d0656e6f2a859e3272a0384
Soni <Sonikaur1661@gmail.com>
2023-06-25 19:21:12 +0530

    firstcommit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const router = express.Router();
+import { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+ } from '../controller/userController.js';
+ import { protect } from '../middleware/authMiddleware.js';
+
+
+router.post('/', authUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router
+   .route('/profile')
+   .get(protect, getUserProfile)
+   .put(protect, updateUserProfile); 
+
+
+
+
+export default router;
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from './components/Header.jsx';
+import { Container } from "react-bootstrap";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <ToastContainer />
+    <Container classname='my-2'>
+      <Outlet/>
+
+    </Container>
+     
+    </>
+  );
+};
+export default App;
